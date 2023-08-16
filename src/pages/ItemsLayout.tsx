8e55b66846a1675f289192f259348352b42354a8
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-08-16 16:59:03 -0300

    createItems

--- src/pages/ItemsLayout.tsx
+++ src/pages/ItemsLayout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import './ItemsLayout.scss'
 
 export default function ItemsLayout(){
 
 
     return (
-        <main>
+        <main className="main-items">
             <h1>stock items</h1>
             <div className="tabs">
                 <Link to='/items'>Items</Link>
